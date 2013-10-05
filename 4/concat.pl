45750a199d681fdf694bd833456cdd49914c51b3
aidan <aidan@felsic.(none)>
2013-10-05 16:34:05 +1000

    1 hour: worked on string concat and more advanced print statements

---
+++ 4/concat.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+# This program tests string concatenation
+# Found on the cs2041 forums as a demo program
+
+$h = "hello"; 
+$w = "world";
+
+print "$h $w\n";
+
+$hw = $h." ".$w;
+
+print "$hw\n";
---
+++ 4/concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7 -u
+
+# This program tests string concatenation
+# Found on the cs2041 forums as a demo program
+
+h = "hello"
+w = "world"
+
+print h, w
+
+hw = h+" "+w
+
+print hw
